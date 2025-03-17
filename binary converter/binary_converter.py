31d4e8bf8ced7474121eb5aa93dd1fcf85f8b18b
jara <jaroslava.bianko@gmail.com>
2025-03-17 19:12:52 +0100

    Update Binary Converter

--- binary converter/binary_converter.py
+++ binary converter/binary_converter.py
@@ -4,12 +4,19 @@ def to_bin(num):
     
     return to_bin(num//2) + str(num%2)
     
-    
+def to_bin_negative(num):
+    if num < 0:
+        return str(1) + to_bin(num*-1)
+    else:
+        return str(0) + to_bin(num)
 
 def to_dec(num):
     digits = [int(d) for d in str(num)]
     return sum(d * 2**(len(digits) - 1 - enum) for enum, d in enumerate(digits))
         
+def to_dec_negative(num):
+    digits=[int(d) for d in str(num)]
+    return (-1 if digits[0] == 1 else 1) * sum(d * 2**(len(digits) - 1 - enum) for enum, d in enumerate(digits[1:]))
 
 def main():
     print("Enter a number and the type of number through space.",
@@ -17,7 +24,7 @@ def main():
           "\n\n101110 2",
           "\n  or",
           "\n34 10",
-          "\n\n(2 for binary number, 10 for decimal):")
+          "\n\n(2 for binary number, 10 for decimal, add minus before (-2/-10) if it\'s negative):")
     
     while True:
         try:
@@ -31,8 +38,16 @@ def main():
         if not any(x > 1 for x in map(int, str(num))):
             return to_dec(num)
     
+    if num_type == -2:
+        if not any(x > 1 for x in map(int, str(num))):
+            return to_dec_negative(num)
+    
     if num_type == 10:
-        return to_bin(num)
+        if num >= 0:
+            return to_bin(num)
+    
+    if num_type == -10:
+            return to_bin_negative(num)
     
     print("Something went wrong. Try again.")
     return main()
