6fddfd9ef61af47a6dfa74d1f9fedf34aa0f5f3c
jara <jaroslava.bianko@gmail.com>
2025-03-17 20:43:50 +0100

    Update Binary Converter

--- binary converter/binary_converter.py
+++ binary converter/binary_converter.py
@@ -1,3 +1,5 @@
+#converter
+
 def to_bin(num):
     if num//2 == 0:
         return str(num%2)
@@ -5,10 +7,17 @@ def to_bin(num):
     return to_bin(num//2) + str(num%2)
     
 def to_bin_negative(num):
-    if num < 0:
-        return str(1) + to_bin(num*-1)
+    number = to_bin(num*-1).replace('1', '2').replace('0', '1').replace('2', '0')
+    if number[-1] == '0':
+        number = number[:-1] + '1'
     else:
-        return str(0) + to_bin(num)
+        for n in range(len(number[:-1])):
+            number[-n] = number[:-n] + '0' + number[-n+1:]
+            if number[-n-1] == '0':
+                number[-n-1] = number[:-n-1] + '1' + number[-n:]
+                break
+            
+    return '1' + number
 
 def to_dec(num):
     digits = [int(d) for d in str(num)]
@@ -16,7 +25,7 @@ def to_dec(num):
         
 def to_dec_negative(num):
     digits=[int(d) for d in str(num)]
-    return (-1 if digits[0] == 1 else 1) * sum(d * 2**(len(digits) - 1 - enum) for enum, d in enumerate(digits[1:]))
+    return (-1 if digits[0] == 1 else 1) * 2**(len(digits)-1) + sum(d * 2**(len(digits) - 2 - enum) for enum, d in enumerate(digits[1:]))
 
 def main():
     print("Enter a number and the type of number through space.",
@@ -24,7 +33,7 @@ def main():
           "\n\n101110 2",
           "\n  or",
           "\n34 10",
-          "\n\n(2 for binary number, 10 for decimal, add minus before (-2/-10) if it\'s negative):")
+          "\n\n(2 for binary number, 10 for decimal, add minus before (-2) if the binary number is negative):")
     
     while True:
         try:
@@ -45,8 +54,7 @@ def main():
     if num_type == 10:
         if num >= 0:
             return to_bin(num)
-    
-    if num_type == -10:
+        if num < 0:
             return to_bin_negative(num)
     
     print("Something went wrong. Try again.")
